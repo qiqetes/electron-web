71626ed640838d8d03d3e41da6e0d954288077b7
Enrique Llopis <enrique@bestcycling.es>
2022-08-04 13:11:06 +0200

    added 2.bc.com flavour

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 // config.js
-const env = process.env.NODE_ENV;
+const env = process.env.FLAVOUR ? "devprod" : process.env.NODE_ENV;
 console.log("Entorno de desarrollo:", env.toUpperCase());
+if (process.env.FLAVOUR) {
+  console.log("Flavour: 2.bestcycling.com");
+}
 
 const development = {
   WEBBASE: "http://localhost:8080",
@@ -9,13 +12,18 @@ const development = {
 
 const production = {
   WEBBASE: "https://bestcycling.com",
-  WEBBASE2: "http://localhost:8080",
+  LOGIN_PATH: "/app/#/login",
+};
+
+const devprod = {
+  WEBBASE: "https://2.bestcycling.com",
   LOGIN_PATH: "/app/#/login",
 };
 
 const config = {
   development,
   production,
+  devprod,
 };
 
 module.exports = config[env];
