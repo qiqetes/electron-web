b3d48a1caae6524085c62e15dba743b32bcf61c6
Enric Llopis <qiqetes@gmail.com>
2022-10-20 05:48:36 +0200

    fix: win working

--- src/@types/binData.d.ts
+++ src/@types/binData.d.ts
@@ -1,14 +1,11 @@
 interface BinData {
-  binaryPath: string,
-  macPath: string,
-  win32Path: string,
-  win64Path: string,
-  currentSystem: SystemsAllowed
+  binaryPath: string;
+  macPath: string;
+  win32Path: string;
+  // win64Path: string,
+  currentSystem: SystemsAllowed;
 }
 
-type BinTypes =
-  | 'ffmpeg'
+type BinTypes = "ffmpeg" | "ffmpeg.exe";
 
-type SystemsAllowed =
-  | 'MAC'
-  | 'WIN'
+type SystemsAllowed = "MAC" | "WIN";
--- src/data/binData.ts
+++ src/data/binData.ts
@@ -1,55 +1,76 @@
-import * as os from 'os';
-import path from 'path';
-import child_process from 'child_process';
+import * as os from "os";
+import path from "path";
+import child_process from "child_process";
 
 class BinDataModel implements BinData {
+  binaryPath = "";
+  macPath = "mac";
+  win32Path = "win32";
+  // win64Path = "win64";
 
-  binaryPath = '';
-  macPath = 'mac';
-  win32Path = 'win32';
-  win64Path = 'win64';
-
-  currentSystem: SystemsAllowed = 'WIN';
+  currentSystem: SystemsAllowed = "WIN";
 
   constructor() {
     this.binaryPath = this.getBinaryPath();
-    this.currentSystem = this.binaryPath.split('/').reverse()[0] === this.macPath ? 'MAC' : 'WIN';
+    this.currentSystem =
+      this.binaryPath.split("/").reverse()[0] === this.macPath ? "MAC" : "WIN";
   }
 
   getBinaryPath() {
-    if (process.env.NODE_ENV === 'development') {
-      return path.join('bin', this.macPath); 
+    if (process.env.NODE_ENV === "development") {
+      if (os.platform() === "darwin") {
+        return path.join("bin", this.macPath);
+      } else if (os.platform() === "win32") {
+        return path.join("bin", this.win32Path);
+      }
     }
 
     let osPath = this.macPath;
 
     switch (os.platform()) {
-      case 'darwin': osPath = this.macPath;
+      case "darwin":
+        osPath = this.macPath;
         break;
-      case 'win32': osPath = this.win32Path;
+      case "win32":
+        osPath = this.win32Path;
         break;
-      
-      default: osPath = this.win64Path;
+
+      // default:
+      //   osPath = this.win64Path;
     }
 
-    return path.join(process.resourcesPath, 'bin', osPath);
+    return path.join(process.resourcesPath, "bin", osPath);
   }
 
-  executeBinary(command: BinTypes, args: string[], options?: Record<string, unknown>) {
+  executeBinary(
+    command: BinTypes,
+    args: string[],
+    options?: Record<string, unknown>
+  ) {
     const fullCommand = path.join(this.binaryPath, command);
-    const finalCommand = this.currentSystem === 'WIN' ? `\\c ${fullCommand}` : fullCommand;
 
     /**
      * TO-DO: Needs to be tested
      * For windows command should be something like:
      * command => cmd.exe (entry point for command line, not the parameter command)
      * args => ['c/', paramCommand, paramArgs]
-     * 
+     *
      * return child_process.spawn('cmd.exe', ['c/', command, ...args]);
+     *  var ffmpeg = spawn( 'cmd.exe', ['/c',  '"'+ffmpegpath+ '"', '-i', filelist[i], '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-c:a', 'aac', '-y', outfile]));
      */
-    return child_process.spawn(finalCommand, args, options);
-  }
 
+    if (os.platform() === "darwin") {
+      return child_process.spawn(fullCommand, args, options);
+    } else if (os.platform() === "win32") {
+      return child_process.spawn(
+        "cmd.exe",
+        ["/c", fullCommand, ...args],
+        options
+      );
+    } else {
+      throw new Error("Platform not supported");
+    }
+  }
 }
 
 export = BinDataModel;
