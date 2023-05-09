1f7b673aeef44d2d186a16fc102491bace190223
Enrique Llopis <enrique@bestcycling.es>
2023-05-09 15:53:35 +0200

    fix: user-agent header

--- src/helpers/init.ts
+++ src/helpers/init.ts
@@ -129,7 +129,7 @@ const initDB = async () => {
       "Content-Type": "application/vnd.api+json",
       "X-APP-ID": AppData.XAPPID,
       Authorization: AppData.AUTHORIZATION,
-
+      "User-Agent": AppData.USER_AGENT,
     },
     baseURL: "https://apiv2.bestcycling.es/api/v2",
   });
