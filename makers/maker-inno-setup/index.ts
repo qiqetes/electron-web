b49e9a980c7fe2f9f30a3a3c28c8d80156b6aa9d
Jose M. Carbonell <robe5.agf@gmail.com>
2023-06-21 12:35:09 +0200

    cleaning

--- makers/maker-inno-setup/index.ts
+++ makers/maker-inno-setup/index.ts
@@ -1,109 +1,108 @@
-const { MakerBase, MarkerOptions } = require("@electron-forge/maker-base")
-const fs = require("fs-extra")
-const path = require("path")
+const { MakerBase, MarkerOptions } = require("@electron-forge/maker-base");
+const fs = require("fs-extra");
+const path = require("path");
 
-const { spawnSync, execSync } = require("child_process")
-const { zipSync, zip } = require("cross-zip")
-const { promisify } = require("util")
+const { spawnSync, execSync } = require("child_process");
+const { zip } = require("cross-zip");
+const { promisify } = require("util");
 
 class MakerInnoSetup extends MakerBase {
-  name = "InnoSetup"
-  defaultPlatforms = ["win32"]
-  platforms = ["win32"]
+  name = "InnoSetup";
+  defaultPlatforms = ["win32"];
+  platforms = ["win32"];
 
   isSupportedOnCurrentPlatform() {
-    return true
-    this.isInstalled("electron-winstaller")
+    return true;
+    this.isInstalled("electron-winstaller");
   }
   async make({ dir, makeDir, targetArch, packageJSON, appName, forgeConfig }) {
-    //console.info(dir, makeDir, targetArch, appName, forgeConfig)
-
-    const outPath = path.resolve(makeDir, `innosetup/${targetArch}/${packageJSON.version}`)
+    const outPath = path.resolve(
+      makeDir,
+      `innosetup/${targetArch}/${packageJSON.version}`
+    );
     await this.ensureDirectory(outPath);
 
-    const innoSetupScript = targetArch === "ia32" ? "innosetup32.iss" : "innosetup64.iss"
-    const exe = `SetupBestcycling${targetArch === "ia32" ? "32" : "64"}`
-    const updater = `update-win${targetArch === "ia32" ? "32" : "64"}`
+    const innoSetupScript =
+      targetArch === "ia32" ? "innosetup32.iss" : "innosetup64.iss";
+    const exe = `SetupBestcycling${targetArch === "ia32" ? "32" : "64"}`;
+    const updater = `update-win${targetArch === "ia32" ? "32" : "64"}`;
     const innoSetupConfig = {
       name: "Bestcycling",
       innoSetupScript,
       outputDirectory: outPath,
       exe: exe,
       updater: updater,
-      ...this.config
-    }
+      ...this.config,
+    };
 
+    await createWindowsInstaller(innoSetupConfig);
 
-    await createWindowsInstaller(innoSetupConfig)
+    await signWindowsInstaller(innoSetupConfig);
 
-    await signWindowsInstaller(innoSetupConfig)
-
-    await packagingWindowsUpdater(innoSetupConfig)
+    await packagingWindowsUpdater(innoSetupConfig);
 
     const artifacts = [
       path.resolve(outPath, `${exe}.exe`),
-      path.resolve(outPath, `${updater}.zip`)
-    ]
+      path.resolve(outPath, `${updater}.zip`),
+    ];
 
-    return artifacts
+    return artifacts;
   }
 }
 
-
 async function createWindowsInstaller(config) {
-  let cmd = 'C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe'
+  const cmd = "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe";
 
   const args = [
     `/O${config.outputDirectory}`,
     `/F${config.exe}`,
     config.innoSetupScript,
-  ]
+  ];
 
-  console.info("building...")
-  return spawnSync(cmd, args, { stdio: "pipe" })
+  console.info("building...");
+  return spawnSync(cmd, args, { stdio: "pipe" });
 }
 
 async function signWindowsInstaller(config) {
-  const cmd = "signtool.exe"
+  const cmd = "signtool.exe";
 
   let args = [
     "sign",
     config.signParams,
-    path.resolve(config.outputDirectory, `${config.exe}.exe`)
-  ].filter(a => a)
-  console.info(args)
+    path.resolve(config.outputDirectory, `${config.exe}.exe`),
+  ].filter((a) => a);
 
-  console.info("signing...")
+  console.info("signing...");
 
-  const signResult = spawnSync(cmd, args, { stdio: "pipe" })
+  const signResult = spawnSync(cmd, args, { stdio: "pipe" });
   console.info(signResult.output.toString());
 
   args = [
     "verify",
     "/pa",
-    path.resolve(config.outputDirectory, `${config.exe}.exe`)
-  ]
-  console.info("validating...")
-  const validateResult = spawnSync(cmd, args, { stdio: "pipe" })
+    path.resolve(config.outputDirectory, `${config.exe}.exe`),
+  ];
+  console.info("validating...");
+  const validateResult = spawnSync(cmd, args, { stdio: "pipe" });
   console.info(validateResult.output.toString());
 }
 
 async function packagingWindowsUpdater(config) {
-  console.info("compressing...")
-  const source = path.resolve(config.outputDirectory, `${config.exe}.exe`)
-  const tmpdir = path.resolve(config.outputDirectory, "temp")
-  const temp = path.resolve(tmpdir, `Bestcycling TV.exe`)
-  const updater = path.resolve(config.outputDirectory, `${config.updater}.zip`)
+  console.info("compressing...");
+  const source = path.resolve(config.outputDirectory, `${config.exe}.exe`);
+  const tmpdir = path.resolve(config.outputDirectory, "temp");
+  const temp = path.resolve(tmpdir, `Bestcycling TV.exe`);
+  const updater = path.resolve(config.outputDirectory, `${config.updater}.zip`);
 
   if (!fs.existsSync(tmpdir)) {
     fs.mkdirSync(tmpdir);
   }
-  fs.copyFileSync(source, temp)
-  await promisify(zip)(tmpdir, updater)
+  fs.copyFileSync(source, temp);
+  await promisify(zip)(tmpdir, updater);
 
   if (fs.existsSync(tmpdir)) {
-    fs.rmdirSync(tmpdir, { recursive: true, maxRetries: 3 })
+    fs.rmdirSync(tmpdir, { recursive: true, maxRetries: 3 });
   }
 }
 
-exports.default = MakerInnoSetup
+exports.default = MakerInnoSetup;
